86f0c9a5ea752c2642b8c18c60467cc9e008068d
MathisLoubiere <mathis.loubiere@epfedu.fr>
2025-12-17 16:58:33 +0100

    Projet Threes V1

--- src/main/java/com/mycompany/projet_threes/FenetreJeu.java
+++ src/main/java/com/mycompany/projet_threes/FenetreJeu.java
@@ -6,7 +6,11 @@ package com.mycompany.projet_threes;
 
 import javax.swing.JLabel;
 import javax.swing.JPanel;
-
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
+import java.util.Random;
     /*
  *
  * @author scham
@@ -16,57 +20,194 @@ public class FenetreJeu extends javax.swing.JFrame {
     
     private static final java.util.logging.Logger logger = java.util.logging.Logger.getLogger(FenetreJeu.class.getName());
 
-    private JPanel panelGrille;
-    private JLabel[] cases;
-
+    
     /**
      * Creates new form FenetreJeu
      */
+    
+    private static final int TAILLE = 4;
+    private JLabel[][] cases = new JLabel[TAILLE][TAILLE];
+ 
+    private int[][] grille = new int[TAILLE][TAILLE];
+    private Random random = new Random();
+
     public FenetreJeu() {
-        initComponents();
-        this.getContentPane().setLayout(new java.awt.BorderLayout());
+        setTitle("Threes");
+        setSize(420, 420);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setLocationRelativeTo(null);
 
-        // Création du panel 4x4
-        panelGrille = new JPanel();
-        panelGrille.setLayout(new java.awt.GridLayout(4, 4, 5, 5)); // 4 lignes, 4 colonnes, gaps 5px
+        JPanel panel = new JPanel(new GridLayout(4, 4, 5, 5));
+        panel.setBackground(Color.DARK_GRAY);
 
-        cases = new JLabel[16];
+        Font font = new Font("Arial", Font.BOLD, 28);
 
-        for (int i = 0; i < 16; i++) {
-        cases[i] = new JLabel("", javax.swing.SwingConstants.CENTER);
-        cases[i].setBorder(javax.swing.BorderFactory.createLineBorder(java.awt.Color.BLACK));
-        cases[i].setFont(new java.awt.Font("Arial", java.awt.Font.BOLD, 24));
-        panelGrille.add(cases[i]);
-        
-        
-        
-        
-}
-
-// On ajoute le panel au centre du JFrame
-    this.getContentPane().add(panelGrille, java.awt.BorderLayout.CENTER);
-
-    jeu = new Jeu();
-    jeu.ajouterTuile();
-    jeu.ajouterTuile();
-
-    mettreAJourGrille();
-
-// On demande à Swing de recalculer la fenêtre
-    this.revalidate();
-    this.repaint();
-  }  
- private void mettreAJourGrille() {
-        int[][] g = jeu.getGrille();
-        for (int i = 0; i < 16; i++) {
-            int ligne = i / 4;
-            int colonne = i % 4;
-            int valeur = g[ligne][colonne];
-            cases[i].setText(valeur == 0 ? "" : String.valueOf(valeur));
+        for (int i = 0; i < TAILLE; i++) {
+            for (int j = 0; j < TAILLE; j++) {
+                cases[i][j] = new JLabel("", SwingConstants.CENTER);
+                cases[i][j].setFont(font);
+                cases[i][j].setOpaque(true);
+                cases[i][j].setBackground(Color.LIGHT_GRAY);
+                panel.add(cases[i][j]);
+            }
         }
+
+        add(panel);
+        initialiserJeu();
+        rafraichir();
+
+        addKeyListener(new KeyAdapter() {
+            @Override
+            public void keyPressed(KeyEvent e) {
+                boolean deplacement = false;
+
+                switch (e.getKeyCode()) {
+                    case KeyEvent.VK_LEFT -> deplacement = gauche();
+                    case KeyEvent.VK_RIGHT -> deplacement = droite();
+                    case KeyEvent.VK_UP -> deplacement = haut();
+                    case KeyEvent.VK_DOWN -> deplacement = bas();
+                }
+
+                if (deplacement) {
+                    ajouterNombre();
+                    rafraichir();
+                }
+            }
+        });
+
+        setFocusable(true);
     }
 
-    
+    private void initialiserJeu() {
+        ajouterNombre();
+        ajouterNombre();
+    }
+
+    private void ajouterNombre() {
+        int valeur = random.nextBoolean() ? 1 : 2;
+
+        int i, j;
+        do {
+            i = random.nextInt(4);
+            j = random.nextInt(4);
+        } while (grille[i][j] != 0);
+
+        grille[i][j] = valeur;
+    }
+
+    private boolean fusion(int a, int b) {
+        return (a == 1 && b == 2) || (a == 2 && b == 1) || (a >= 3 && a == b);
+    }
+
+    private int valeurFusion(int a, int b) {
+        return a + b;
+    }
+
+    private boolean gauche() {
+        boolean bouge = false;
+        for (int i = 0; i < 4; i++) {
+            for (int j = 1; j < 4; j++) {
+                if (grille[i][j] != 0) {
+                    int k = j;
+                    while (k > 0 && grille[i][k - 1] == 0) {
+                        grille[i][k - 1] = grille[i][k];
+                        grille[i][k] = 0;
+                        k--;
+                        bouge = true;
+                    }
+                    if (k > 0 && fusion(grille[i][k - 1], grille[i][k])) {
+                        grille[i][k - 1] = valeurFusion(grille[i][k - 1], grille[i][k]);
+                        grille[i][k] = 0;
+                        bouge = true;
+                    }
+                }
+            }
+        }
+        return bouge;
+    }
+
+    private boolean droite() {
+        boolean bouge = false;
+        for (int i = 0; i < 4; i++) {
+            for (int j = 2; j >= 0; j--) {
+                if (grille[i][j] != 0) {
+                    int k = j;
+                    while (k < 3 && grille[i][k + 1] == 0) {
+                        grille[i][k + 1] = grille[i][k];
+                        grille[i][k] = 0;
+                        k++;
+                        bouge = true;
+                    }
+                    if (k < 3 && fusion(grille[i][k + 1], grille[i][k])) {
+                        grille[i][k + 1] = valeurFusion(grille[i][k + 1], grille[i][k]);
+                        grille[i][k] = 0;
+                        bouge = true;
+                    }
+                }
+            }
+        }
+        return bouge;
+    }
+
+    private boolean haut() {
+        boolean bouge = false;
+        for (int j = 0; j < 4; j++) {
+            for (int i = 1; i < 4; i++) {
+                if (grille[i][j] != 0) {
+                    int k = i;
+                    while (k > 0 && grille[k - 1][j] == 0) {
+                        grille[k - 1][j] = grille[k][j];
+                        grille[k][j] = 0;
+                        k--;
+                        bouge = true;
+                    }
+                    if (k > 0 && fusion(grille[k - 1][j], grille[k][j])) {
+                        grille[k - 1][j] = valeurFusion(grille[k - 1][j], grille[k][j]);
+                        grille[k][j] = 0;
+                        bouge = true;
+                    }
+                }
+            }
+        }
+        return bouge;
+    }
+
+    private boolean bas() {
+        boolean bouge = false;
+        for (int j = 0; j < 4; j++) {
+            for (int i = 2; i >= 0; i--) {
+                if (grille[i][j] != 0) {
+                    int k = i;
+                    while (k < 3 && grille[k + 1][j] == 0) {
+                        grille[k + 1][j] = grille[k][j];
+                        grille[k][j] = 0;
+                        k++;
+                        bouge = true;
+                    }
+                    if (k < 3 && fusion(grille[k + 1][j], grille[k][j])) {
+                        grille[k + 1][j] = valeurFusion(grille[k + 1][j], grille[k][j]);
+                        grille[k][j] = 0;
+                        bouge = true;
+                    }
+                }
+            }
+        }
+        return bouge;
+    }
+
+    private void rafraichir() {
+        for (int i = 0; i < 4; i++) {
+            for (int j = 0; j < 4; j++) {
+                if (grille[i][j] == 0) {
+                    cases[i][j].setText("");
+                    cases[i][j].setBackground(Color.LIGHT_GRAY);
+                } else {
+                    cases[i][j].setText(String.valueOf(grille[i][j]));
+                    cases[i][j].setBackground(new Color(255 - grille[i][j] * 5, 200, 120));
+                }
+            }
+        }
+    }
 
     /**
      * This method is called from within the constructor to initialize the form.
